8d56c56ff3fdf5e1c34b1af0d25a040bf41eb8ee
unknown <you@example.com>
2019-07-10 14:40:26 +0900

    postコントローラーのビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources :posts
+  resources :homes
+  resources :genre
+  	resource :comments
+  	resource :favorites
+
+  	get "/" => "homes#top"
 end
