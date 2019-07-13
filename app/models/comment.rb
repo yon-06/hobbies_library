558e5b62da55b260a019895860e293278ba45a65
unknown <you@example.com>
2019-07-13 12:04:18 +0900

    モデル周りの調整

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+
+	belongs_to :psot
+	belongs_to :user
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+
+	belongs_to :user
+	belongs_to :genre
+
+	has_many :comments, dependent: :destroy
+	has_many :favorites, dependent: :destroy
+
+	attachment :post_image
 end
