d87bf65af3d7ebba402d46558752f716cf15242f
unknown <you@example.com>
2019-07-13 11:47:57 +0900

    genreのビュー作成

--- app/controllers/genre_controller.rb
+++ app/controllers/genre_controller.rb
@@ -1,2 +1,23 @@
 class GenreController < ApplicationController
+
+	def index
+	end
+
+	def art_show
+	end
+
+	def craft_show
+	end
+
+	def life_show
+	end
+
+	def motion_show
+	end
+
+	def other_show
+	end
+
+	def study_show
+	end
 end
